0ab1801a887f5850a71714a98bff578ccd3aefb3
Robert Stratton <bobby@middesk.com>
2020-12-30 20:20:55 -0800

    Finish solution

--- src/bin/day_13.rs
+++ src/bin/day_13.rs
@@ -29,7 +29,7 @@ fn part1(input: &str) -> u64 {
 fn part2(input: &str) -> u64 {
     let mut lines = input.lines();
     let _earliest_possible_departure_time = lines.next().unwrap().parse::<u64>().unwrap();
-    let busses = lines
+    let mut busses = lines
         .next()
         .unwrap()
         .split(',')
@@ -38,19 +38,27 @@ fn part2(input: &str) -> u64 {
         .map(|(idx, s)| (idx as u64, s.parse::<u64>().unwrap()))
         .collect::<Vec<(u64, u64)>>();
 
-    for (offset, frequency) in busses.sort_by_key(|(_, frequency)| std::cmp::Reverse(frequency)) {}
-    let (offset, greatest_departure_cadence) = busses
-        .iter()
-        .max_by_key(|(_, departure_cadence)| departure_cadence)
-        .unwrap();
 
-    let times_to_try =
-        ((*greatest_departure_cadence - *offset)..).step_by(*greatest_departure_cadence as usize);
+    // First find t such that t % 863 == (863 - 72) % 863 counting by 1
+    // Next find t such that t % 431 == (431 - 41) % 431 counting by 863 * 1
+    // Next find t such that t % 41 == (41 - 0) % 41 counting by 431 * 863 * 1
 
-    println!("{:?}", busses);
-    println!("{:?} -- {:?}", offset, greatest_departure_cadence);
-    println!("{:?}", times_to_try.take(10).collect::<Vec<u64>>());
-    0
+    let mut modulus = 1u64;
+    let mut current_time = 0u64;
+    busses.sort_by_key(|(_, frequency)| std::cmp::Reverse(*frequency));
+    
+    for (offset, frequency) in busses {
+        let target_value_modulo_frequency = (frequency - (offset  % frequency)) % frequency;
+        for candidate_time in (current_time..).step_by(modulus as usize) {
+            if candidate_time % frequency == target_value_modulo_frequency {
+                current_time = candidate_time;
+                modulus *= frequency;
+                break;
+            }
+        }
+    }
+    
+    current_time
     // for time in times_to_try {
     //     let all_departures_align = busses
     //         .iter()
